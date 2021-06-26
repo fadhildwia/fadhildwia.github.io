1d46008cbc706412f1b435cd752a3fad431b0012
Fadhilah Dwi A <fadhilahdwi.a@gmail.com>
2021-06-26 20:53:37 +0700

    new fiture

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,8 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', () => {
     nav.classList.toggle('slide');
 })
+
+const menu = document.querySelector('section#brand ul li');
+menu.addEventListener('click', function() {
+    menu.style.paddingTop = '30px';
+})
