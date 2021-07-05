be6da08311dd305b41f8120adf999d367a26db98
Fadhilah Dwi A <fadhilahdwi.a@gmail.com>
2021-07-05 10:56:53 +0700

    new feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,6 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', () => {
     nav.classList.toggle('slide');
 });
+
+const logo = document.querySelector('.brand');
+logo.style.cursor='pointer';
