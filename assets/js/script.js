e694fc67156592ac906f0bfd50adf9cfcdd01e6f
Fadhilah Dwi A <fadhilahdwi.a@gmail.com>
2021-07-05 13:08:20 +0700

    new feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,18 @@ menuToggle.addEventListener('click', () => {
 
 const logo = document.querySelector('.brand');
 logo.style.cursor='pointer';
+
+
+const navbars = document.querySelector('.navbar').querySelectorAll('a');
+const animations = document.querySelectorAll('.title');
+console.log(navbars);
+console.log(animations);
+navbars.forEach(element => {
+    element.addEventListener('click', function(){
+        navbars.forEach(navbar=>navbar.classList.remove('active'))
+        
+
+        this.classList.add('active');
+    })
+});
+
