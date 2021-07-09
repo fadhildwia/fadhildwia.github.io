4e05bfe52cc289947b3925ade009aa8d72b0925b
Fadhilah Dwi A <fadhilahdwi.a@gmail.com>
2021-07-09 21:57:25 +0700

    new feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,21 @@
-const menuToggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul');
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
 
-menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('slide');
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("slide");
 });
 
-const logo = document.querySelector('.brand');
-logo.style.cursor='pointer';
+const logo = document.querySelector(".brand");
+logo.style.cursor = "pointer";
 
+const navbars = document.querySelector(".navbar").querySelectorAll("a");
+const animations = document.querySelectorAll(".title");
+// console.log(navbars);
+// console.log(animations);
+navbars.forEach((element) => {
+  element.addEventListener("click", function () {
+    navbars.forEach((navbar) => navbar.classList.remove("active"));
 
-const navbars = document.querySelector('.navbar').querySelectorAll('a');
-const animations = document.querySelectorAll('.title');
-console.log(navbars);
-console.log(animations);
-navbars.forEach(element => {
-    element.addEventListener('click', function(){
-        navbars.forEach(navbar=>navbar.classList.remove('active'))
-        
-
-        this.classList.add('active');
-    })
+    this.classList.add("active");
+  });
 });
-
